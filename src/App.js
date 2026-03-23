32a0fd1344b0dfa392babf280b5fbd912a63b66f
NeilMathew33 <mathew.neil33@gmail.com>
2026-03-22 17:42:52 -0700

    Add/Delete Student

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import EmployeeInformation from './container/EmployeeInformation';
+import DeleteEmployee from './container/DeleteEmployee';
+import SearchEmployee from './container/SearchEmployee';
+import ViewEmployee from './container/ViewEmployee';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <EmployeeInformation>
-      </EmployeeInformation>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<EmployeeInformation />} />
+        <Route path="/delete" element={<DeleteEmployee />} />
+        <Route path="/search" element={<SearchEmployee />} />
+        <Route path="/ViewAll" element={<ViewEmployee />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+      export default App;
---
+++ src/container/ViewEmployee.jsx
@@ -0,0 +1,123 @@
+import React from 'react'
+import EmployeeNavBar from './EmployeeNavBar'
+
+const ViewEmployee = () => {
+  return (
+    <div>
+<EmployeeNavBar/>
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRweYNDO6_gA3B8P8GLXZlfdrHO1-v0BSsroA&s" class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Neil</h5>
+                    <p class="card-text">Computer Engineer</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRweYNDO6_gA3B8P8GLXZlfdrHO1-v0BSsroA&s" class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Neil</h5>
+                    <p class="card-text">Computer Engineer</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRweYNDO6_gA3B8P8GLXZlfdrHO1-v0BSsroA&s" class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Neil</h5>
+                    <p class="card-text">Computer Engineer</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRweYNDO6_gA3B8P8GLXZlfdrHO1-v0BSsroA&s" class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Neil</h5>
+                    <p class="card-text">Computer Engineer</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRweYNDO6_gA3B8P8GLXZlfdrHO1-v0BSsroA&s" class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Neil</h5>
+                    <p class="card-text">Computer Engineer</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRweYNDO6_gA3B8P8GLXZlfdrHO1-v0BSsroA&s" class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Neil</h5>
+                    <p class="card-text">Computer Engineer</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRweYNDO6_gA3B8P8GLXZlfdrHO1-v0BSsroA&s" class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Neil</h5>
+                    <p class="card-text">Computer Engineer</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRweYNDO6_gA3B8P8GLXZlfdrHO1-v0BSsroA&s" class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Neil</h5>
+                    <p class="card-text">Computer Engineer</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRweYNDO6_gA3B8P8GLXZlfdrHO1-v0BSsroA&s" class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Neil</h5>
+                    <p class="card-text">Computer Engineer</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                <div class="card">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRweYNDO6_gA3B8P8GLXZlfdrHO1-v0BSsroA&s" class="card-img-top" alt="..." />
+                  <div class="card-body">
+                    <h5 class="card-title">Neil</h5>
+                    <p class="card-text">Computer Engineer</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default ViewEmployee
